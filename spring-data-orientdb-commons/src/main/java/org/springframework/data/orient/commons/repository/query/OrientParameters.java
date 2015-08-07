0df9382d50ab33398cf7d0b7733c63e69cc030ae
Paulo Pires <pjpires@gmail.com>
2015-08-07 14:17:56 +0100

    Implemented OrientParameters forEach and spliterator functions.

--- spring-data-orientdb-commons/src/main/java/org/springframework/data/orient/commons/repository/query/OrientParameters.java
+++ spring-data-orientdb-commons/src/main/java/org/springframework/data/orient/commons/repository/query/OrientParameters.java
@@ -5,31 +5,39 @@ import org.springframework.data.orient.commons.repository.OrientSource;
 import org.springframework.data.repository.query.Parameters;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
 public class OrientParameters extends Parameters<OrientParameters, OrientParameter> {
 
     private final int sourceIndex;
-    
+    private final List<OrientParameter> originals; // parent's parameters attribute is not accessible
+
     private OrientParameters(List<OrientParameter> originals) {
         super(originals);
-        
+
+        this.originals = originals;
+
         int clusterIndexTemp = -1;
 
         for (int i = 0; i < originals.size(); i++) {
             OrientParameter original = originals.get(i);
             clusterIndexTemp = original.isSource() ? i : -1;
         }
-        
+
         sourceIndex = clusterIndexTemp;
     }
 
     public OrientParameters(Method method) {
         super(method);
-        
+
+        originals = new ArrayList<>();
+
         List<Class<?>> types = Arrays.asList(method.getParameterTypes());
-        
+
         sourceIndex = types.indexOf(OrientSource.class);
     }
 
@@ -48,12 +56,22 @@ public class OrientParameters extends Parameters<OrientParameters, OrientParamet
     protected OrientParameters createFrom(List<OrientParameter> parameters) {
         return new OrientParameters(parameters);
     }
-    
+
     public int getSourceIndex() {
         return sourceIndex;
     }
-    
+
     public boolean hasSourceParameter() {
         return sourceIndex != -1;
+    }
+
+    @Override
+    public void forEach(Consumer<? super OrientParameter> action) {
+        originals.forEach(action);
+    }
+
+    @Override
+    public Spliterator<OrientParameter> spliterator() {
+        return originals.spliterator();
     }
 }
